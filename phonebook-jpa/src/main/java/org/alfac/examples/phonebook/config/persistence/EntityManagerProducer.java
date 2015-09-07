6a35de6ce583c59c2d40eed8c3307406370cad6d
lord-breetai <ivan.alban@gmail.com>
2015-09-06 20:36:03 -0400

    Data source configuration

---
+++ phonebook-jpa/src/main/java/org/alfac/examples/phonebook/config/persistence/EntityManagerProducer.java
@@ -0,0 +1,10 @@
+package org.alfac.examples.phonebook.config.persistence;
+
+import javax.enterprise.context.ApplicationScoped;
+
+/**
+ * @author ivan.
+ */
+@ApplicationScoped
+public class EntityManagerProducer {
+}
---
+++ phonebook-jpa/src/main/java/org/alfac/examples/phonebook/config/persistence/PhonebookDS.java
@@ -0,0 +1,33 @@
+package org.alfac.examples.phonebook.config.persistence;
+
+/**
+ * @author ivan.
+ */
+
+import javax.annotation.Resource;
+import javax.annotation.sql.DataSourceDefinition;
+import javax.inject.Named;
+import javax.sql.DataSource;
+
+/**
+ * @author ivan.
+ */
+@DataSourceDefinition(
+        name = "java:app/jdbc/phonebookDS",
+        transactional = true,
+        url = "jdbc:hsqldb:mem:phonebookDS",
+        className = "org.hsqldb.jdbcDriver",
+        user = "sa",
+        password = ""
+)
+
+@Named
+public class PhonebookDS {
+
+    @Resource(lookup = "java:app/jdbc/phonebookDS")
+    private DataSource dataSource;
+
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+}
